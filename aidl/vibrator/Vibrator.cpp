68f229315ae35c7ee3ed6f7bd42dd7a1bbbaba80
AnierinB <anierin@evolution-x.org>
2022-10-28 21:38:27 +0000

    aidl: vibrator: Use the same sysfs writes as click for tick
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- aidl/vibrator/Vibrator.cpp
+++ aidl/vibrator/Vibrator.cpp
@@ -497,8 +497,8 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es, const std
             break;
         case Effect::TICK:
             ledVib.write_value(LED_DEVICE "/rtp", "0");
-            ledVib.write_value(LED_DEVICE "/vmax", "80");
-            ledVib.write_value(LED_DEVICE "/waveform_index", "2");
+            ledVib.write_value(LED_DEVICE "/vmax", "1600");
+            ledVib.write_value(LED_DEVICE "/waveform_index", "1");
             ledVib.write_value(LED_DEVICE "/brightness", "1");
             ledVib.write_value(LED_DEVICE "/rtp", "0");
             break;
