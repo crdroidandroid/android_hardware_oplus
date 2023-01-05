f401decd106bf421c2defd28964bb603f9f3ea3a
jabashque <jabashque@gmail.com>
2023-01-05 11:42:46 -0800

    fingerprint: set dimlayer_hbm on finger up/down instead of auth/cancel
    When dimlayer_hbm is enabled, HWC ends up disabling all color
    transformations, including Night Light. Since we enable/disable
    dimlayer_hbm on authenticate/cancel, this means that whenever the UDFPS
    circle is present, Night Light ends up temporarily turning off. This is
    especially not helpful on the lock screen, where the UDFPS circle is
    active all the time.
    
    Instead of turning on dimlayer_hbm during the entire time that the UDFPS
    circle is showing, let's turn on dimlayer_hbm only when the user is
    pressing down on the circle. In doing so, the color transformations will
    be disabled for only when the user is touching the circle. We will still
    disable dimlayer_hbm on cancel anyway, just in case that wasn't already
    set to off by the time cancel() is called.
    
    However, when enrolling a new fingerprint, the user is going to be
    tapping on the UDFPS circle many, many times, and the user may prefer to
    just keep dimlayer on for the entire duration. Add `isEnrolling` to
    track that, and don't change dimlayer state while that is set to true.
    
    Change-Id: I9ed1a6b5839e39b8f70ff4af4ebe8d1bc66ffb98
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- hidl/fingerprint/BiometricsFingerprint.h
+++ hidl/fingerprint/BiometricsFingerprint.h
@@ -126,6 +126,7 @@ class BiometricsFingerprint : public IBiometricsFingerprint,
     sp<V2_1::IBiometricsFingerprintClientCallback> mClientCallback;
 
     int mOplusDisplayFd;
+    bool isEnrolling;
 };
 
 }  // namespace implementation
