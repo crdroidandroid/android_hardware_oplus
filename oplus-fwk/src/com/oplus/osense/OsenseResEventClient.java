59bad04ccefe3c139f5af9d8f089fbd290d432e4
cjh1249131356 <cjh1249131356@gmail.com>
2024-11-06 20:36:28 +0530

    oplus-fwk: Add missing classes for oplus camera
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- oplus-fwk/src/com/oplus/osense/OsenseResEventClient.java
+++ oplus-fwk/src/com/oplus/osense/OsenseResEventClient.java
@@ -1,20 +1,24 @@
 package com.oplus.osense;
 
+import com.oplus.osense.eventinfo.EventConfig;
+import com.oplus.osense.eventinfo.OsenseEventCallback;
+
 public class OsenseResEventClient {
 
-    private static volatile OsenseResEventClient sInstance;
-
-    private OsenseResEventClient() {
-    }
+    private static OsenseResEventClient sInstance;
 
     public static OsenseResEventClient getInstance() {
         if (sInstance == null) {
-            synchronized (OsenseResEventClient.class) {
-                if (sInstance == null) {
-                    sInstance = new OsenseResEventClient();
-                }
-            }
+            sInstance = new OsenseResEventClient();
         }
         return sInstance;
+    }
+
+    public int registerEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
+    }
+
+    public int unregisterEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
     }
 }
