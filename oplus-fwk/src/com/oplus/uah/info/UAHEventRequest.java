e6e834f7af0b4347dccdcf969354f431cc627d4f
cjh1249131356 <cjh1249131356@gmail.com>
2024-11-06 20:36:28 +0530

    oplus-fwk: Add missing classes for oplus camera
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ oplus-fwk/src/com/oplus/uah/info/UAHEventRequest.java
@@ -0,0 +1,9 @@
+package com.oplus.uah.info;
+
+import java.util.ArrayList;
+
+public class UAHEventRequest {
+
+    public UAHEventRequest(int eventId, String sceneName, int timeout,
+            ArrayList<UAHResourceInfo> list) {}
+}
