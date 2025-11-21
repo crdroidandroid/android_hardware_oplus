0714b1a5ec45e5834e80aa388bdf0968daab7954
Pranav Vashi <neobuddy89@gmail.com>
2025-11-22 01:33:22 +0530

    Add toggle for ltpo features
    Credits to @RealJohnGalt
    https://github.com/yaap/device_oneplus_sm8650-common/commit/91b1f9bc76fc850d22b14103dcbec5a30646eeed
    
    Requires:
    https://github.com/crdroidandroid/android_device_oneplus_sm8650-common/commit/3ad2d76f277fcc5b0a29b52da746df86df12289c
    https://github.com/yaap/kernel_oneplus_sm8650-modules/commit/05a6fe5b9d12188847f6bf7cec680ae24aef7c85
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ ltpo/src/com/crdroid/ltpo/oplus/Startup.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2018-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.ltpo.oplus;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserManager;
+import android.util.Log;
+
+public class Startup extends BroadcastReceiver {
+
+    private static final String TAG = Startup.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String action = intent != null ? intent.getAction() : null;
+        if (action == null) return;
+
+        UserManager um = context.getSystemService(UserManager.class);
+        if (um == null) {
+            Log.d(TAG, "User service not ready");
+            return;
+        }
+
+        if (!um.isPrimaryUser()) {
+            Log.d(TAG, "Not running as the primary user");
+            return;
+        }
+
+        Log.d(TAG, "Restoring LTPO settings");
+        LTPOSettings.restoreLTPOSetting(context);
+    }
+}
