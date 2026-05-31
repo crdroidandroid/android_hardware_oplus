e613f0c1ca5260bf9c98f010f12622f366a98e86
spkal01 <kalligeross@gmail.com>
2026-05-31 16:39:24 +0300

    oplus-fwk: Update shims from OP13

---
+++ oplus-fwk/src/android/graphics/OplusBaseRenderNode.java
@@ -0,0 +1,5 @@
+package android.graphics;
+
+public class OplusBaseRenderNode {
+    // Dummy stub
+}
---
+++ oplus-fwk/src/com/oplus/osense/task/BgRunningCallback.java
@@ -0,0 +1,9 @@
+package com.oplus.osense.task;
+
+public class BgRunningCallback {
+    public void requestRunningTaskInfo(int pid, int uid) {
+    }
+
+    public void cancelRunningTaskInfo(int pid, int uid) {
+    }
+}
